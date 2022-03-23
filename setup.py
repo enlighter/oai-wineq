de41386adef817a296906bbc29461813cf8167e2
Sushovan Mandal <sushovan.mandal@oracle.com>
2022-03-23 17:11:10 +0530

    make proper python package and run tox on package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version="0.0.0",
+    description="Wine package demo",
+    author="enlighter",
+    packages=find_packages(),
+    license="MIT"
+)
