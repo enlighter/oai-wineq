ee1ec0343bdfe7daf5cc9389d2cd93b37ace9391
Sushovan Mandal <sushovan.mandal@oracle.com>
2022-03-24 10:48:44 +0530

    raise not in range test failure

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_generic():
     a = 30
     b = 40
@@ -8,3 +11,18 @@ def test_mul():
     b = 40
     c = a*b
     assert c >= 100
+
+
+class NotInRange(Exception):
+    def ___init__(self, message="value not in range"):
+        # self.input_ = input_
+        self.message = message
+        super().__init__(self.message)
+
+
+def test_generic1():
+    a = 5
+    with pytest.raises(ValueError):
+        if a not in range(10, 20):
+            raise NotInRange
+        
