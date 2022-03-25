d1ae1824fc3142665e253b45a3a8f8caef40c361
Sushovan Mandal <sushovan.mandal@oracle.com>
2022-03-25 14:35:15 +0530

    integrate mlflow

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -9,10 +9,12 @@ import numpy as np
 import argparse
 import joblib
 import json
+from urllib.parse import urlparse
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from get_data import read_params
+from src.get_data import read_params
+import mlflow
 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
@@ -26,12 +28,12 @@ def train_and_evaluate(config_path):
     test_data_path = config['split_data']['test_path']
     train_data_path = config['split_data']['train_path']
     random_state = config['base']['random_state']
-    model_dir = config['model_dir']
+    # model_dir = config['model_dir']
     alpha = config['estimators']['ElasticNet']['params']['alpha']
     l1_ratio = config['estimators']['ElasticNet']['params']['l1_ratio']
     target = config['base']['target_col']
-    scores_file = config['reports']['scores']
-    params_file = config['reports']['params']
+    # scores_file = config['reports']['scores']
+    # params_file = config['reports']['params']
 
     train = pd.read_csv(train_data_path, sep=',')
     test = pd.read_csv(test_data_path, sep=',')
@@ -42,35 +44,54 @@ def train_and_evaluate(config_path):
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
 
-    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
-    lr.fit(train_x, train_y)
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
 
-    predicted_qualities = lr.predict(test_x)
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    mlflow.set_tracking_uri(remote_server_uri)
+    mlflow.set_experiment(mlflow_config["experiment_name"])
 
-    print("ElasticNet model (alpha=%f, l1_ratio=%f) : " %(alpha, l1_ratio))
-    print("RMSE : %s" % rmse)
-    print("MAE: %s" % mae)
-    print("R2 : %s" % r2)
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+        lr.fit(train_x, train_y)
 
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
-    joblib.dump(lr, model_path)
+        predicted_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    with open(scores_file, "w") as f:
-        scores = {
-            'rmse': rmse,
-            'mae': mae,
-            "r2": r2
-        }
-        json.dump(scores, f, indent=4)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
 
-    with open(params_file, "w") as f:
-        params = {
-            'alpha': alpha,
-            'l1_ratio': l1_ratio
-        }
-        json.dump(params, f, indent=4)
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        if tracking_url_type_store != 'file':
+            mlflow.sklearn.log_model(
+                lr,
+                'model',
+                registered_model_name=mlflow_config["registered_model_name"]
+            )
+        else:
+            mlflow.sklearn.load_model(lr, 'model')
+
+    # os.makedirs(model_dir, exist_ok=True)
+    # model_path = os.path.join(model_dir, "model.joblib")
+    # joblib.dump(lr, model_path)
+
+    # with open(scores_file, "w") as f:
+    #     scores = {
+    #         'rmse': rmse,
+    #         'mae': mae,
+    #         "r2": r2
+    #     }
+    #     json.dump(scores, f, indent=4)
+
+    # with open(params_file, "w") as f:
+    #     params = {
+    #         'alpha': alpha,
+    #         'l1_ratio': l1_ratio
+    #     }
+    #     json.dump(params, f, indent=4)
 
 
 if __name__ == '__main__':
